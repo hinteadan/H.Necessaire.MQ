973cd2440b1995407d88e3c7a7d2a84978cd9eec
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-09-14 09:21:29 +0300

    OD: Play with MQ on Azure

--- Src/H.Necessaire.MQ/Playground/H.Necessaire.MQ.Playground.AspNetPlay/AppWireup.cs
+++ Src/H.Necessaire.MQ/Playground/H.Necessaire.MQ.Playground.AspNetPlay/AppWireup.cs
@@ -1,5 +1,7 @@
 ﻿using H.Necessaire.Runtime;
 using H.Necessaire.Runtime.Integration.NetCore;
+using System;
+using System.Linq;
 
 namespace H.Necessaire.MQ.Playground.AspNetPlay
 {
@@ -7,9 +9,16 @@ namespace H.Necessaire.MQ.Playground.AspNetPlay
     {
         public override ImAnApiWireup WithEverything()
         {
+            Type qdActionProcessingHostedServiceDaemonType 
+                = typeof(ImADaemon)
+                .GetAllImplementations(typeof(NetCoreLoggingExtensions).Assembly)
+                .Single(x => x.Name == "QdActionProcessingHostedServiceDaemon")
+                ;
+
             return
                 base
                 .WithEverything()
+                .With(x => x.Unregister(qdActionProcessingHostedServiceDaemonType))
                 .With(x => x.Register<DependencyGroup>(() => new DependencyGroup()))
                 .With(x => x.Register<Runtime.SqlServer.SqlServerRuntimeDependencyGroup>(() => new Runtime.SqlServer.SqlServerRuntimeDependencyGroup()))
                 ;
