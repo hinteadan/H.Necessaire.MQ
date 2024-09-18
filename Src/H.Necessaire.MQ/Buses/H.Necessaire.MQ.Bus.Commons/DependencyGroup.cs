5b77484f8049703f36ea2180a5a9e233812cdbe6
Dan Hintea <dan.hintea@qubiz.com>
2024-09-18 14:36:09 +0300

    OD: MQ Enhancements

---
+++ Src/H.Necessaire.MQ/Buses/H.Necessaire.MQ.Bus.Commons/DependencyGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace H.Necessaire.MQ.Bus.Commons
+{
+    internal class DependencyGroup : ImADependencyGroup
+    {
+        public void RegisterDependencies(ImADependencyRegistry dependencyRegistry)
+        {
+
+        }
+    }
+}
---
+++ Src/H.Necessaire.MQ/Buses/H.Necessaire.MQ.Bus.Commons/🤝Friends.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("H.Necessaire.MQ.Bus.AzureServiceBus")]
+[assembly: InternalsVisibleTo("H.Necessaire.MQ.Bus.RabbitOrLavinMQ")]
